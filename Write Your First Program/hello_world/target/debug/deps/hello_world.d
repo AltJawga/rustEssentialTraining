f55a5238d81b1b3d6f73fa54a3a23aca21085036
Chris <40653036+AltJawga@users.noreply.github.com>
2024-11-22 11:44:43 -0600

    Created debug and release versions

---
+++ Write Your First Program/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\cda0044\Documents\GitHub\rustEssentialTraining\Write Your First Program\hello_world\target\debug\deps\hello_world.exe: src/main.rs
+
+C:\Users\cda0044\Documents\GitHub\rustEssentialTraining\Write Your First Program\hello_world\target\debug\deps\hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ Write Your First Program/hello_world/target/release/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\cda0044\Documents\GitHub\rustEssentialTraining\Write Your First Program\hello_world\target\release\deps\hello_world.exe: src/main.rs
+
+C:\Users\cda0044\Documents\GitHub\rustEssentialTraining\Write Your First Program\hello_world\target\release\deps\hello_world.d: src/main.rs
+
+src/main.rs:
